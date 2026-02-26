54570a9a5203979da377a3a54d7c51af5ab02a6c
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-26 23:49:10 +0000

    got image urls of particular items | select method explored

--- request_api/fetch_blogs.py
+++ request_api/fetch_blogs.py
@@ -7,4 +7,14 @@ page = requests.get(url)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-response = soup.find_all('ol', class_="row")
+# this ol is the container of all the content
+response = soup.find('ol', class_="row")
+if response:
+    item_cards = response.find_all("article", class_ = "product_pod")
+    for item_card in item_cards[0:5]:
+        link = item_card.select("div.image_container a")
+        print(link)
+else:
+    item_cards = []
+
+
