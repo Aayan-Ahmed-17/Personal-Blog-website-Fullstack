7b403b63365f8845a44b405ba32a6a91c54f63c8
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-26 11:01:38 +0000

    get() through requests module with error handling done

---
+++ request_api/scrape.py
@@ -0,0 +1,20 @@
+import requests
+
+url = "https://books.toscrape.com/"
+
+def scrape(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful
+        print(response.raise_for_status())
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+if __name__ == "__main__":
+    html_content = scrape(url)
+    if html_content:
+        print("Scraping successful!")
+    else:
+        print("Scraping failed.")
