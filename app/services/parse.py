826813b3819ab4b2292f054deb6b31afe4f12b47
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-02 23:19:24 +0000

    pasrsed initial data successfully

---
+++ app/services/parse.py
@@ -0,0 +1,17 @@
+from scrape import get_data, headers, url
+from bs4 import BeautifulSoup
+
+response = get_data(url=url, headers=headers)
+
+blog_container_classes = "div.grid.w-full.grid-cols-1.gap-4.p-2"
+item_card_classes = "div.p-6.bg-white.rounded-xl"
+
+container = None
+
+try:
+  soup = BeautifulSoup(response.content, "html.parser") # type: ignore
+  container = soup.select_one(blog_container_classes)
+
+
+except Exception as e:
+  print(f"Error: {e}")
