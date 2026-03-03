00208ec87595f4c264263aaca5bd1ab8317698b8
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-03 09:06:11 +0000

    img_link, title, update_date extracted

--- app/services/parse.py
+++ app/services/parse.py
@@ -6,6 +6,9 @@ response = get_data(url=url, headers=headers)
 blog_container_select = "div.grid.w-full.grid-cols-1.gap-4.p-2"
 item_card_select = "div.p-6.bg-white.rounded-xl"
 blog_link_select = "div a"
+blog_img_link_select = "div a div img"
+blog_title_select = 'div a h3'
+blog_update_data_select = 'div div div span'
 
 container = None
 
@@ -19,11 +22,15 @@ description
 """
 
 
-def parse_data():
-    try:
-        soup = BeautifulSoup(response.content, "html.parser")  # type: ignore
-        blog_container = soup.select_one(blog_container_select)
-        blog_link = blog_container.select_one(blog_link_select).get("href")  # type: ignore
-        print(blog_link)
-    except Exception as e:
-        print(f"Error: {e}")
+# def parse_data():
+#     try:
+soup = BeautifulSoup(response.content, "html.parser")  # type: ignore
+blog_container = soup.select_one(blog_container_select)
+blog_link = blog_container.select_one(blog_link_select).get("href")  # type: ignore
+img_link = blog_container.select_one(blog_img_link_select).get("src") # type: ignore
+blog_title = blog_container.select_one(blog_title_select).text # type: ignore
+blog_update_data = blog_container.select(blog_update_data_select)[3].text # type: ignore
+
+        # print(blog)
+    # except Exception as e:
+    #     print(f"Error: {e}")
