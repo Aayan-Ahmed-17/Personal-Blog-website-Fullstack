4ef84cf2a6f413d5890b3926ec953ffa3efacea0
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-04 00:09:25 +0000

    mvd routers folder main file into app/ folder

--- app/api/v1/routers/articles/online.py
+++ app/api/v1/routers/articles/online.py
@@ -1,11 +1,12 @@
 from app.services.parse import parse_data
 from app.services.scrape import get_data, url, headers
-from fastapi import FastAPI
+from fastapi import APIRouter
 
-app = FastAPI()
+router = APIRouter()
 
 
-@app.get("/blogs")
+@router.get("/")
 def read_blogs():
     response = get_data(url=url, headers=headers)
     return parse_data(response)
+
--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+from app.api.v1.routers.articles.online import router as online_router
 from fastapi import FastAPI
-from .routers import blogs
 
-app = FastAPI(title="FastAPI Template", description="A simple FastAPI template for building APIs", version="1.0.0")
-app.include_router(blogs.router, prefix="/api/v1/blogs", tags=["Blogs"])
+app = FastAPI(
+    title="FastAPI Template",
+    description="A simple FastAPI template for building APIs",
+    version="1.0.0",
+)
+
+app.include_router(online_router, prefix="/api/v1/blogs", tags=["Online Blogs"])
