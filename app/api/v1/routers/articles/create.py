4b8a3308f92edd4367d1fe4ce1a12d87c4f339e1
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-08 23:18:47 +0000

    rmvd repeatative files | online.py I'll handle it later

--- app/api/v1/routers/articles/create.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.schemas.CreateBlog import CreateBlog
-
-router = APIRouter()
-
-@router.post("/blogs")
-async def create_blog(blog: CreateBlog): # type: ignore
-    return {"message": "Blog created successfully", "blog": blog}
