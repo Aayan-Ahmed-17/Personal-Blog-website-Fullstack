50161e324256319156ea58ef75150b3f1222af03
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-08 21:18:24 +0000

    get and create func for mongodb blog dn | update and delete remain | serialize file created

--- app/services/crud/blog_crud.py
+++ app/services/crud/blog_crud.py
@@ -1,6 +1,17 @@
 from app.db.mongodb import blog_collection
+from app.utils.serializer import blog_serializer
 
 
 def create_blog(blog_data: dict):
     result = blog_collection.insert_one(blog_data)
     return str(result.inserted_id)
+
+
+def get_all_blogs():
+    blogs = blog_collection.find()
+    return [blog_serializer(blog) for blog in blogs]
+
+
+def get_blog(blog_id):
+    blog = blog_collection.find_one({"_id": blog_id})
+    return blog_serializer(blog)
---
+++ app/utils/serializer.py
@@ -0,0 +1,7 @@
+def blog_serializer(blog) -> dict:
+    return {
+        "id": str(blog["_id"]),
+        "title": blog["title"],
+        "content": blog["content"],
+        "author": blog["author"],
+    }
