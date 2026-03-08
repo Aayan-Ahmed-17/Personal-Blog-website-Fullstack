7fe43459000206608c3efe3ba54b334ba4fda7a9
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-08 13:38:34 +0000

    routes setup | need file structure | setup functionality

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from app.api.v1.routers.articles.online import router as online_router
-from app.api.v1.routers.articles.create import router as create_router
+from app.api.v1.endpoints.blogs import router as blogs_router
+from app.api.v1.endpoints.scraper import router as scraper_router
 from fastapi import FastAPI
 
 app = FastAPI(
@@ -8,5 +8,5 @@ app = FastAPI(
     version="1.0.0",
 )
 
-app.include_router(online_router, prefix="/api/v1/blogs", tags=["Online Blogs"])
-app.include_router(create_router, prefix="/api/v1/blogs", tags=["Create Blogs"])
+app.include_router(blogs_router, prefix="/api/v1")
+app.include_router(scraper_router, prefix="/api/v1")
