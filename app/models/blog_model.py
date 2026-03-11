ecea3f7e79fe38824d261d2c412ef6846688607e
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-11 07:54:58 +0000

    blog model updated

--- app/models/blog_model.py
+++ app/models/blog_model.py
@@ -1,28 +1,15 @@
 from datetime import datetime
-from typing import Optional
+from pydantic import BaseModel, Field, EmailStr
 
 
-class BlogModel:
-    def __init__(
-        self,
-        author_name: str,
-        title: str,
-        description: str,
-        author_email: Optional[str] = None,
-    ):
-        self.author_name = author_name
-        self.author_email = author_email
-        self.title = title
-        self.description = description
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-
-    def to_dict(self):
-        return {
-            "author_name": self.author_name,
-            "author_email": self.author_email,
-            "title": self.title,
-            "description": self.description,
-            "created_at": self.created_at,
-            "updated_at": self.updated_at,
-        }
+class BlogModel(BaseModel):
+    author_name: str = Field(
+        ..., min_length=3, max_length=16, description="Enter AUthor name"
+    )
+    author_email: EmailStr | None = Field(
+        None, description="An optional email address for contact."
+    )
+    title: str = Field(..., min_length=3, description="Enter Blog Title Here...")
+    description: str = Field(..., description="Enter Blog Description Here...")
+    created_at : datetime = Field(default_factory=datetime.now)
+    updated_at : datetime = Field(default_factory=datetime.now)
