d7c666e6bc1d7a2004df57a90978894c90afd1d6
aayan-ahmed-17 <aayanahmedtejani@gmail.com>
2026-04-13 22:42:24 +0500

    img_link not found error resolved

--- app/services/scraper/selectors.py
+++ app/services/scraper/selectors.py
@@ -2,7 +2,7 @@ BLOG_GRID_CONTAINER = "div.grid.w-full.grid-cols-1.gap-4.p-2"  # container for a
 BLOG_CONTAINER = "div.p-6.bg-white.rounded-xl"  # container for each blog post | through BLOG_GRID_CONTAINER | apply for loop
 
 BLOG_LINK = "div a.group"  # link of each blog post | through BLOG_CONTAINER
-IMG_TAG = "div a div img.w-full.h-auto.transition-all.transition.duration-200.ease-in-out.lightense-target"  # image tag of each blog post | through BLOG_CONTAINER
+IMG_TAG = "img.w-full.h-auto.transition-all"  # image tag of each blog post | through BLOG_CONTAINER
 TITLE_TAG = "div a h3"  # title of each blog post | through BLOG_CONTAINER
 DATE_TAG = "div div div span.text-gray-500"  # date tag of each blog post | through BLOG_CONTAINER | apply for loop to get 2 spans, then select the second one for the latest date
 DESCRIPTION_TAG = "div div p.mt-6.leading-normal.text-gray-600.line-clamp-3"  # description of each blog post | through BLOG_CONTAINER
