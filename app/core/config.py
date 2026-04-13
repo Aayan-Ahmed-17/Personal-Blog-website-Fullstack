609c0ecd8b99d31b8767919088d1395ba3194544
aayan-ahmed-17 <aayanahmedtejani@gmail.com>
2026-04-13 19:11:24 +0500

    temp commit

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 USERNAME = os.getenv("USERNAME", "")
 PASSWORD = os.getenv("PASSWORD", "")
@@ -10,7 +10,7 @@ URL = os.getenv("URL")
 
 # Build MONGO_URI with SSL certificate workaround
 if USERNAME and PASSWORD:
-    MONGO_URI = f"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.r2krjac.mongodb.net/{DB_NAME}?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
-    print("✓ MONGO_URI successfully constructed.")
+    MONGO_URI = f"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.r2krjac.mongodb.net/?appName=Cluster0"
+    print("✓ MONGO_URI successfully constructed.", MONGO_URI)
 else:
     raise ValueError("❌ ERROR: USERNAME or PASSWORD not found in .env file")
